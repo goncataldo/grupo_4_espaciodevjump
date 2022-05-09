020adb6432049b70bdf1c554c8317a3765aca23b
Kevin Aviles <kevin.aviles@arbusta.net>
2022-05-09 12:03:14 -0300

    comentarios a 2 archivos js

--- js/munuAside.js
+++ js/munuAside.js
@@ -1,8 +1,6 @@
-const $elAside=document.querySelector('.elAside');
-const $menuChiquito=document.querySelector('.menuChiquito');
+const $elAside=document.querySelector('.elAside');/* captura en una constante la etiqueta que tenga el nombre de su clase como ".elAside" */
+const $menuChiquito=document.querySelector('.menuChiquito');/*captura en una constante la etiqueta que tenga el nombre de su clase como ".menuChiquito"  */
 
-$menuChiquito.addEventListener('click',()=>{
-    $elAside.classList.toggle("active");
-    console.log("gola");
-    /* botonMenu.classList.toggle("active"); */
+$menuChiquito.addEventListener('click',()=>{/* al hacer click en el menu chiquito  */
+    $elAside.classList.toggle("active");/* a la etiqueta se le agrega la clase "active" y al hacer click otra ves se le quita*/
 })
